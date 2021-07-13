ce3f07e6598b5f1f1582ae41f4b189588f8573a8
ernesto-m <ernesto.m.marambio@gmail.com>
2021-07-13 19:56:11 +0200

    refactor phpstan en 6

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -2,10 +2,19 @@
 
 namespace Runroom\GildedRose;
 
+use Runroom\GildedRose\Items\Item;
+
 class GildedRose {
 
+    /**
+     * @var array<Item>
+     */
     private array $items;
 
+    /**
+     * GildedRose constructor.
+     * @param array<Item> $items
+     */
     function __construct(array $items) {
         $this->items = $items;
     }
