f7eccd212eaeb2c70ec7cadad941df3a06cf09ee
ernesto-m <ernesto.m.marambio@gmail.com>
2021-07-13 18:47:38 +0200

    Se crea item BackstageItem

---
+++ src/Items/BackstageItem.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Runroom\GildedRose\Items;
+
+
+class BackstageItem extends Item
+{
+    public function __construct($name, $sell_in, $quality)
+    {
+        parent::__construct($name, $sell_in, $quality);
+    }
+
+    public function updateItem()
+    {
+        // TODO: Implement updateItem() method.
+    }
+
+}
